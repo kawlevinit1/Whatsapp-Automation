4a95c18a31c621b445f332493d6195621088c189
kawlevinit1 <kawlevinit@gmail.com>
2020-11-29 18:50:38 +0530

    Initial Commit

--- hello.py
+++ hello.py
@@ -3,12 +3,11 @@ from twilio.rest import Client
 account_sid = 'ACe27cf670a2545c290f01cdce6d0a6e9e' 
 auth_token = 'f0c4322715d34cc8bc0e1168e546d396' 
 client = Client(account_sid, auth_token) 
-def hello():
-
-	message = client.messages.create( 
+ 
+message = client.messages.create( 
                               from_='whatsapp:+14155238886',  
-                              body='Good work ',      
+                              body='Hello! This is an editable text message. You are free to change it and write whatever you like.',      
                               to='whatsapp:+919359184123' 
                           ) 
  
-	print(message.sid)
+print(message.sid)
